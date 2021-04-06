d7784eb534b89f8e683ef82708fbf5e562853498
m-schein <maiaraschein@gmail.com>
2021-04-06 01:01:58 -0300

    Arrow functions

---
+++ arrowFunctions.js
@@ -0,0 +1,13 @@
+//funções anonimas(arrow function)
+const funcao = (param1, param2, param3) => {
+  console.log(param1, param2, param3);
+  return param1 + param2 + param3;
+}
+funcao(1,2,3)
+
+//funções normais 
+function funcao2 (param1, param2, param3) {
+  console.log(param1, param2, param3);
+  return param1 + param2 + param3;
+}
+funcao2(3,3,3)
